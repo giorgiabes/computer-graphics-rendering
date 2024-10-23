2a995704dcff64a465aa67cb3c330467bb9fa15a
Giorgi <abesadze@george.com>
2024-10-23 11:12:40 +0400

    3-9 fullscreen window

--- src/main.c
+++ src/main.c
@@ -20,6 +20,13 @@ bool initialize_window(void)
 		fprintf(stderr, "Error initializing SDL.\n");
 		return false;
 	}
+
+	// Use SDL to query what is the fullscreen max. Width and Height
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
 	// Create a SDL Window
 	window = SDL_CreateWindow(
 			NULL,
@@ -41,6 +48,7 @@ bool initialize_window(void)
 		fprintf(stderr, "Error creating SDL renderer.\n");
 		return false;
 	}
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 	
 	return true;
 }
