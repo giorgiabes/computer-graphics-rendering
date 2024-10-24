e7bc958db08d2281f772aa94fd2cf500f9a36445
Giorgi <abesadze@george.com>
2024-10-24 15:33:17 +0400

    6-1

--- src/main.c
+++ src/main.c
@@ -12,6 +12,9 @@
 // const int N_POINTS = 9 * 9 * 9;
 #define N_POINTS (9 * 9 * 9)
 vec3_t cube_points[N_POINTS]; // 9x9x9 cube
+vec2_t projected_points[N_POINTS];
+
+float fov_factor = 128;
 
 bool is_running = false;
 
@@ -56,20 +59,46 @@ void process_input(void) {
 	}
 }
 
+/**
+ * Function that receives a 3D vector and converts a projected 2D point
+ */
+vec2_t project(vec3_t point) {
+	vec2_t projected_point = {
+		.x = (fov_factor * point.x),
+		.y = (fov_factor * point.y)
+	};
+	return projected_point;
+}
+
 void update(void) {
-	// TODO:
+	for (int i = 0; i < N_POINTS; i++) {
+		vec3_t point = cube_points[i];
+
+		// Project the current point
+		vec2_t projected_point = project(point);
+
+		// Save the projected 2D vector in the array of projected points
+		projected_points[i] = projected_point;
+	}
 }
 
 void render(void) {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
 	draw_grid();
 
-	draw_pixel(20, 20, 0xFFFFFF00);
-	draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+	// Loop all projected points and render them
+	for (int i = 0; i < N_POINTS; i++) {
+		vec2_t projected_point = projected_points[i];
+		draw_rect(
+			projected_point.x + (window_width / 2),
+			projected_point.y + (window_height / 2),
+			8,
+			8,
+			0xFFFFFF00
+		);
+	}
 
 	render_color_buffer();
+	
 	clear_color_buffer(0xFF000000);
 
 	SDL_RenderPresent(renderer);
