35eb131ed7ded996eaee9a4f0bf9f1c41111da9e
Giorgi <abesadze@george.com>
2024-10-24 14:38:51 +0400

    5-5

--- src/main.c
+++ src/main.c
@@ -5,14 +5,20 @@
 #include "display.h"
 #include "vector.h"
 
+
+/**
+ * Declare an array of vectors/points
+ */
+// const int N_POINTS = 9 * 9 * 9;
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS]; // 9x9x9 cube
+
 bool is_running = false;
 
 void setup(void) {
-	// Allocate the required memory in bytes to hold the 
-	// color buffer
+	// Allocate the required memory in bytes to hold the color buffer
 	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
-	// Creatingg a SDL texture that is used to
-	// display the color buffer
+	// Creatingg a SDL texture that is used to display the color buffer
 	color_buffer_texture = SDL_CreateTexture(
 		renderer,
 		SDL_PIXELFORMAT_ARGB8888,
@@ -20,6 +26,19 @@ void setup(void) {
 		window_width,
 		window_height
 	);
+
+	int point_count = 0;
+
+	// Start loading my array of vectors
+	// From -1 to 1 (in this 9x9x9 cube)
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = {.x = x, .y = y, .z = z};
+				cube_points[point_count++] = new_point;
+			}
+		}
+	}
 }
 
 void process_input(void) {
@@ -60,8 +79,6 @@ int main(void) {
 	is_running = initialize_window();
 
 	setup();
-
-	vec3_t myvector = {2.0, 3.0, -4.0};
 
 	while (is_running) {
 		process_input();
